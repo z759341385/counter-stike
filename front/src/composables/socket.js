befcb2fa32b99e1cd351328ec5869b0d110b69bc
Zhanghongchen <zhanghongchen1998@126.com>
2026-05-15 17:06:07 +0800

    1.0

--- front/src/composables/socket.js
+++ front/src/composables/socket.js
@@ -4,7 +4,7 @@
  */
 import { io } from 'socket.io-client'
 
-const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || 'http://localhost:3000'
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || 'http://localhost:3001'
 
 export const socket = io(BACKEND_URL, {
   autoConnect: false,
